a56778c85e85f48b52bb73ea446abe475c96ebbd
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-02-27 21:12:26 +0300

    Basic front page design

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import { Button, Heading, Stack, Text } from "@chakra-ui/react";
+import Search from "../components/Search";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello world!</h1>
+      <Stack
+        spacing={4}
+        justifyContent={"center"}
+        alignItems={"center"}
+        py={"20"}
+      >
+        <Heading>الباحث الفقهي</Heading>
+        <Text>خدمة تسهل الوصول إلى المسائل الفقهية حسب المذهب</Text>
+        <Search />
+        <Button colorScheme="blue">تصفح المسائل</Button>
+      </Stack>
     </>
   );
 }
