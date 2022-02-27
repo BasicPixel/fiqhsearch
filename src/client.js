f7cfeb8ca702e525d3e290404de8248745db28e5
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-02-27 07:33:58 +0300

    Setup Firebase and react-firebase-hooks

---
+++ src/client.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAoSMzIQXY3oFAohXaOC2VKbQ1HIIYlnPw",
+  authDomain: "fiqhsearch.firebaseapp.com",
+  projectId: "fiqhsearch",
+  storageBucket: "fiqhsearch.appspot.com",
+  messagingSenderId: "661722850024",
+  appId: "1:661722850024:web:7513daa847fcc6a86e1f5d",
+  measurementId: "G-JXFXS9VE3X",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
