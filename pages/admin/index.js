85bc70dba974af63d453650da3d16207b78092b1
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-03-02 21:35:39 +0300

    Move admin page to new dir

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -2,9 +2,9 @@ import { Heading } from "@chakra-ui/react";
 import { getAuth } from "firebase/auth";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import AdminInterface from "../components/AdminInterface";
-import Login from "../components/Login";
-import app from "../src/client";
+import AdminInterface from "../../components/AdminInterface";
+import Login from "../../components/Login";
+import app from "../../src/client";
 
 const Admin = () => {
   const [user, loading] = useAuthState(getAuth(app));
