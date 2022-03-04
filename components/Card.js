7303f8ffcf52efc6d184dd115e59a696fa3b7e66
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-03-04 16:57:40 +0300

    Create Card component

---
+++ components/Card.js
@@ -0,0 +1,20 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const Card = ({ children }) => {
+  return (
+    <Box
+      padding={8}
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <h1>{children}</h1>
+    </Box>
+  );
+};
+
+export default Card;
