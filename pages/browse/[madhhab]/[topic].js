3993bc015b3a85e7d5c062a14a80bed036b6b763
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-03-06 19:20:07 +0300

    Add backlink to topic page

--- pages/browse/[madhhab]/[topic].js
+++ pages/browse/[madhhab]/[topic].js
@@ -1,10 +1,19 @@
+import { Heading, Stack, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
 const Topic = () => {
   const router = useRouter();
 
-  return <div>{router.query.topic}</div>;
+  return (
+    <Stack spacing={4}>
+      <NextLink href={`/browse/${router.query.madhhab}`} passHref>
+        <Link>تصفح جميع المسائل...</Link>
+      </NextLink>
+      <Heading>{router.query.topic}</Heading>
+    </Stack>
+  );
 };
 
 export default Topic;
