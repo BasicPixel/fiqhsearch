8ad1d1a84822891106df9d677a0d5b8fb9bb9114
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-03-29 22:07:18 +0300

    Fix router bug

--- pages/browse/index.js
+++ pages/browse/index.js
@@ -1,12 +1,18 @@
 import { Heading } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import useLocalStorage from "../../hooks/useLocalStorage";
+import { useEffect } from "react";
 
 const BrowseHome = () => {
   const [madhhab] = useLocalStorage("madhhab", "hanbali");
   const router = useRouter();
 
-  router.push(`browse/${madhhab}`);
+  useEffect(() => {
+    () => {
+      router.push(`browse/${madhhab}`);
+    };
+  }, []);
+
   return <Heading textAlign={"center"}>جارٍ التحميل...</Heading>;
 };
 
