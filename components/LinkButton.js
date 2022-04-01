12a79eeb0b5d6fc95934983a235c4b641f58d14e
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-04-01 10:21:53 +0300

    Create Link button component

---
+++ components/LinkButton.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+import { Button } from "@chakra-ui/react";
+
+const LinkButton = ({ children, href, icon, colorScheme }) => {
+  return (
+    <Link href={href} passHref>
+      <Button colorScheme={colorScheme} rightIcon={icon}>
+        {children}
+      </Button>
+    </Link>
+  );
+};
+
+export default LinkButton;
