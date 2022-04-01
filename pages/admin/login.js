935b894458bd83209eb8c979a1e690a44461f9cd
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-04-01 07:02:59 +0300

    Refactor using useUser hook

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -6,18 +6,13 @@ import { Auth } from "@supabase/ui";
 import supabase from "src/client";
 
 const LoginPage = () => {
-  const [user, setUser] = React.useState(null);
-
+  const { user } = Auth.useUser();
   const router = useRouter();
-
-  supabase.auth.onAuthStateChange((e) => {
-    if (e === "SIGNED_IN") setUser(supabase.auth.user());
-  });
 
   if (user) router.push("/admin");
   return (
     <div dir="ltr">
-      <Auth supabaseClient={supabase} />
+      <Auth supabaseClient={supabase} redirectTo={"/admin"} />
     </div>
   );
 };
